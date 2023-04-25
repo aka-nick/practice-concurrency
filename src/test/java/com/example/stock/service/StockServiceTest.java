d88a5490727d0d24bf5012852eec296748ccf2e6
aka-nick <bechalrang@gmail.com>
2023-04-25 16:12:53 +0900

    재고 감소 기능 테스트 케이스 추가

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.stock.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.save(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void stock_decrease() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(99L);
+
+    }
+}
